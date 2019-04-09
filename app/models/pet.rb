95f18198d0ab3256c1a2600a7b3038352d52c13b
Jasper D'haene <jasper.dhaene@gmail.com>
2019-04-09 16:49:10 +0200

    added favorite food restrictions

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,17 +4,18 @@ class Pet < ApplicationRecord
 end
 
 class Cat < Pet
-
+  validates :favorite_food, inclusion: { in: %w(milk water bread ) }
 end
 
 class Dog < Pet
-
+  validates :favorite_food, inclusion: { in: %w(water meat ) }
 end
 
 class Mouse < Pet
+  validates :favorite_food, inclusion: { in: %w(water cheese ) }
 
 end
 
 class Horse < Pet
-
+  validates :favorite_food, inclusion: { in: %w(carrot grass water ) }
 end
