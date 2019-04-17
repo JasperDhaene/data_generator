aff2e8e52fd2f5e5b3411b1acbdba55f6540ad6c
Jasper D'haene <jasper.dhaene@gmail.com>
2019-04-17 17:51:12 +0200

    Able to kill pets now

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,3 +1,12 @@
 class PetsController < ApplicationController
 
+  def update
+    @pet = Pet.find(params[:id])
+    if @pet.update(date_of_death: Date.today)
+      redirect_to user_path(@pet.user)
+    else
+      # panic?
+    end
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+=begin
   resources :users do
     resources :pets
     resources :fridges
   end
+=end
+
+  resources :users do
+    resources :fridges
+  end
+  resources :pets
+
+
 
   root 'users#index'
 end
