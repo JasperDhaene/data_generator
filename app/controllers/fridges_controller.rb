0233c7fd60215f41be2f778e238705896b1d65c8
Jasper D'haene <jasper.dhaene@gmail.com>
2019-04-17 17:24:35 +0200

    Add fridge to user

--- app/controllers/fridges_controller.rb
+++ app/controllers/fridges_controller.rb
@@ -1,6 +1,30 @@
 class FridgesController < ApplicationController
   def show
     @user = User.find(params[:user_id])
-    @fridge = @user.fridge
+    @fridge = Fridge.find(params[:id])
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @fridge = Fridge.new(fridge_params)
+    @fridge.user = @user
+    @user.fridge = @fridge
+
+    if @fridge.save && @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def new
+    @user = User.find(params[:user_id])
+    @fridge = Fridge.new
+  end
+
+  private
+
+  def fridge_params
+    params.require(:fridge).permit(%i(brand last_technical_check))
   end
 end
--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,5 +1,3 @@
 class PetsController < ApplicationController
-  def edit
-    @pet = Pet.find(params[:id])
-  end
+
 end
