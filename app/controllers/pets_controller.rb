f953d8117745931e75193db999c1bf66b5d7d7a3
Jasper D'haene <jasper.dhaene@gmail.com>
2019-04-18 09:38:23 +0200

    Pet#show

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,5 +1,9 @@
 class PetsController < ApplicationController
 
+  def show
+    @pet = Pet.find(params[:id])
+  end
+
   def update
     @pet = Pet.find(params[:id])
     if @pet.update(date_of_death: Date.today)
